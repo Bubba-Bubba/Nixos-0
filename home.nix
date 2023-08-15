f4c1f6faa0a22c559ab9d9c67b00112a46805d7a
BriefNCounter <marcusdurston@yandex.com>
2023-08-15 21:00:51 +0200

    aliases_1

--- home.nix
+++ home.nix
@@ -13,6 +13,24 @@
   which
   ];
 
+shellAliases = {
+      cf = "cd /etc/nixos && sudo hx configuration.nix";
+      fl = "cd /etc/nixos && sudo hx flake.nix";
+      hm = "cd /etc/nixos && sudo hx home.nix";
+      rb = "cd /etc/nixos && sudo sudo nixos-rebuild --flake .#My_Nix switch";
+      c = "clear";
+      g0 = "sudo git init";
+      g1 = "sudo git add .";
+      g3 = "sudo git push -u origin main";
+      xx = "reboot";
+      s = "sudo -s";
+      pd = "cd ~/Programming/Python";      
+
+
+      }
+
+
+
   home.stateVersion = "23.05";
 
   # Let home Manager install and manage itself.

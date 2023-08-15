c43f8ba21e5b896e3ba02261145bdf4bfbdccb3a
BriefNCounter <marcusdurston@yandex.com>
2023-08-15 17:49:24 +0200

    first settings

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "FLAKE - Marcus's NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # home-manager, used for managing user configuration
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+helix.url = "github:helix-editor/helix/23.05"; 
+  };
+
+outputs = { self, nixpkgs, ... }@inputs : {
+    nixosConfigurations = {
+      # TODO please change the hostname to your own
+      My_Nix = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+                  specialArgs = inputs;
+
+        
+        modules = [
+          ./configuration.nix
+
+        ];
+      };
+    };
+  };
+}
+

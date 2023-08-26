888fd1a8c1f6ed97a9486e30ffaccae835e74007
BriefNCounter <marcusdurston@yandex.com>
2023-08-26 08:49:09 +0200

    nixvim-3

--- home.nix
+++ home.nix
@@ -4,6 +4,31 @@
   home.username = "marcus";
   home.homeDirectory = "/home/marcus";
 
+programs.nixvim = {
+    enable = true;
+
+    # Configure neovim options...
+    options = {
+      relativenumber = true;
+      incsearch = true;
+    };
+
+    # ...mappings...
+    maps.normal = {
+      "<C-s>" = ":w<CR>";
+      "<esc>" = { action = ":noh<CR>"; silent = true; };
+    };
+
+    # ... and plugins
+    plugins = {
+      telescope.enable = true;
+      harpoon = {  # Hi Prime :)
+        enable = true;
+        keymaps.addFile = "<leader>a";
+      };
+    };
+  };
+
 
   home.packages = with pkgs; [
   tree
